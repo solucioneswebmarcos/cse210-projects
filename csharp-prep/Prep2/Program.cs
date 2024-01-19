7920188a572faa9721a179745eae15b38e68e446
solucioneswebmarcos <marcossud185@gmail.com>
2024-01-19 20:10:29 +0100

    first commit

---
+++ csharp-prep/Prep2/Program.cs
@@ -0,0 +1,43 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("what is your grade pecentage? ");
+        string answer = Console.ReadLine();
+        int pecent = inyt.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >=80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+        if (percent >= 70)
+        {
+            Console.WriteLine("You Passed");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+    }
+}
